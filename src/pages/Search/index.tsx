fadc2e97d53b56c3808f0080410286ee4a8d8575
thiago-vcosta <thiago.vcosta@uol.com.br>
2021-02-11 15:35:28 -0300

    Project finished

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -32,23 +32,31 @@ const Search = () => {
     html_url: ''
   });
 
+  
 
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (search != "") {
     setIsLoading(true);
     setSubmitDone(true);
     fetch(`https://api.github.com/users/${search}`)
       .then(response => response.json())
       .then(userResponse => setUserData(userResponse))
       .finally(() => setIsLoading(false));
+    } else {
+      alert('Favor digitar um usuário');
+    }
   }
 
+
+      
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value)
   }
 
   return (
+    
     <div className="search-container">
       <form onSubmit={handleSubmit}>
         <div className="search-form">
@@ -67,9 +75,8 @@ const Search = () => {
         </div>
       </form>
 
-      {isLoading ? <ObjLoaders /> : []}
-
-      {submitDone ?
+      {isLoading ? <ObjLoaders /> : 
+      submitDone ?
         <SearchResult company={userData.company} public_repos={userData.public_repos}
           followers={userData.followers} following={userData.following} blog={userData.blog} location={userData.location}
           created_at={userData.created_at} avatar_url={userData.avatar_url} html_url={userData.html_url} />
@@ -78,5 +85,7 @@ const Search = () => {
     </div>
   );
 };
+
+
 
 export default Search;
